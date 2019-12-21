ac1c53b9415fc4a0444164108e003b3acca31db6
User <user@Users-MacBook-Pro-235.local>
2019-12-20 23:45:26 -0600

    first commit

---
+++ Task5.py
@@ -0,0 +1,6 @@
+classroom1 = int(input("Students1:"))
+classroom2 = int(input("Students2:"))
+classroom3 = int(input("Students3:"))
+
+result = ((classroom1 + classroom2 + classroom3) / 2)
+print(round(result))
